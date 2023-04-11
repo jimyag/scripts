97c675b25dc573cc10af675193ca51e7d91aafab
jimyag <jiaminyag@gmail.com>
2023-04-11 20:06:34 +0800

    update(ubuntu): init shell

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+# 修改 hostname
+if [ -n "$1" ]; then
+  sudo hostnamectl set-hostname "$1"
+  echo "The new hostname will take effect after rebooting."
+fi
 
 # 判断是否有 sudo 权限
 if ! groups ${USER} | grep -qE '\bsudo\b|\broot\b' ; then
-    echo "请确保您拥有 sudo 权限"
+    echo "Please ensure that you have sudo privileges."
     exit 1
 fi
-
 
 # 判断是否为 Ubuntu 系列
 if [ -f /etc/lsb-release ]; then
@@ -14,19 +18,17 @@ if [ -f /etc/lsb-release ]; then
     if [ "$DISTRIB_ID" = "Ubuntu" ]; then
         echo "System is Ubuntu"
     else
-        echo "暂不支持非 Ubuntu 系列系统"
+        echo "At the moment, only Ubuntu-based systems are supported."
         exit 1
     fi
 else
-    echo "暂不支持非 Ubuntu 系列系统"
+    echo "At the moment, only Ubuntu-based systems are supported."
     exit 1
 fi
 
-
-
 # 判断是否为64位系统
 if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ]; then
-    echo "Error: 暂不支持非64位系统"
+    echo "Currently, only 64-bit systems are supported."
     exit 1
 fi
 
@@ -41,29 +43,22 @@ sudo apt-get update && sudo  apt-get upgrade -y
 # 安装必要的软件
 sudo apt-get install -y wget curl unzip git htop vnstat neovim zsh hstr
 
-
 # 切换默认终端为 zsh
 chsh -s $(which zsh)
 
+# 安装 ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# 配置 hstr
 # refer https://github.com/dvorka/hstr/blob/master/INSTALLATION.md#ubuntu
 hstr --show-configuration >> ~/.bashrc && . ~/.bashrc
 
+# 安装 ohmyzsh 的插件
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting extract)/' ~/.zshrc
 
-
-# 修改当前的 hostname 重启之后生效
-sudo hostnamectl set-hostname aws3
-
-# 切换到 root 用户
-sudo su
-
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting extract)
-
-
-## 下载gost
+# 下载 gost
 curl -L https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz | gunzip -c > gost && chmod +x gost &&  sudo mv gost /usr/local/bin/
 
 # 删除 Please login as the user “ubuntu” rather than the user “root”
